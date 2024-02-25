f0b9b45588056eb6572dbb0296eb09afee0c238a
Richard So <richardso2021@gmail.com>
2024-02-25 11:59:15 -0500

    Revert "remove nonfunctional "Edit" menu" (needed for copy/paste)
    This partially reverts commit 4ddd55fd60bf52ba404ad6a4a30ae065dd525142.

--- src/gui/src/main/index.js
+++ src/gui/src/main/index.js
@@ -81,6 +81,18 @@ function createWindow() {
       ],
     },
     {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "selectAll" },
+      ],
+    },
+    {
       label: "View",
       submenu: [
         { role: "reload" },
